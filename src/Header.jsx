24b1dd44c28966a515ca51b8a32da51ebf5f51d5
HaroldMars <abejar199@gmail.com>
2025-05-10 10:44:23 +0800

    bisan unsa lang na update

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+    return(
+        <div className="bg-amber-950 border-2 ">
+            <header>
+                <h1> JHMVA </h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                    
+                    </ul>
+                </nav>
+                <hr></hr>
+            </header>
+        </div>
+    );
+}
+
+export default Header
